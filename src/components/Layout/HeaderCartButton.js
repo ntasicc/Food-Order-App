e9b438958b2c1c687479f2141a6a164f365b2661
ntasicc <ntasicc@gmail.com>
2022-03-14 23:13:48 +0100

    Animation for every item that is being added to a cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,30 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const ctx = useContext(CartContext);
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
