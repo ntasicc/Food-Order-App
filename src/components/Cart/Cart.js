a8e79df2373c0761b42123b92e232bb42a9e0962
ntasicc <ntasicc@gmail.com>
2022-03-14 23:03:48 +0100

    Adding more items or removing them form a cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,49 @@
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const ctx = useContext(CartContext);
+  const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+  const hasItems = ctx.items.length > 0;
+
+  const cartItemRemoveHanlder = (id) => {
+    ctx.removeItem(id);
+  };
+
+  const cartItemAddHanlder = (item) => {
+    ctx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {ctx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHanlder.bind(null, item.id)}
+          onAdd={cartItemAddHanlder.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
   return (
     <Modal onClose={props.onClose}>
+      {cartItems}
       <div className={classes.total}>
         <span>Total amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
-          {" "}
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
