941bfe615f22f295f8f352a55368326d170b938c
ntasicc <ntasicc@gmail.com>
2022-03-14 21:12:28 +0100

    Rendering cart modal conditionaly with states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
+
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const closeCartHandler = () => {
+    setShowCart(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      {showCart && <Cart onClose={closeCartHandler} />}
+      <Header onShowCartClick={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
