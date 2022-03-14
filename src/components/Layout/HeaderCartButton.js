6fd20bca7783482a18e1786e3382ee3b4fc4483b
ntasicc <ntasicc@gmail.com>
2022-03-14 21:29:48 +0100

    displays the number of items in the cart through context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>
+        {ctx.items.reduce((currNumber, item) => {
+          return currNumber + item.amount;
+        }, 0)}
+      </span>
     </button>
   );
 };
