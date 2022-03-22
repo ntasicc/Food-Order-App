d0f5c0f995f6951168cf39a324d9ea01fba07c55
ntasicc <ntasicc@gmail.com>
2022-03-22 21:33:05 +0100

    Checkout form for order

---
+++ src/components/Cart/CheckoutForm/Checkout.js
@@ -0,0 +1,127 @@
+import useInput from "../../../hooks/use-input";
+import classes from "./Checkout.module.css";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: cityValue,
+    isValid: cityIsValid,
+    hasError: cityHasError,
+    valueChangeHandler: cityChangeHandler,
+    inputBlurHandler: cityBlurHandler,
+    reset: resetCity,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: streetValue,
+    isValid: streetIsValid,
+    hasError: streetHasError,
+    valueChangeHandler: streetChangeHandler,
+    inputBlurHandler: streetBlurHandler,
+    reset: resetStreet,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: postalValue,
+    isValid: postalIsValid,
+    hasError: postalHasError,
+    valueChangeHandler: postalChangeHandler,
+    inputBlurHandler: postalBlurHandler,
+    reset: resetPostal,
+  } = useInput(isFiveChars);
+
+  let formIsValid = false;
+
+  if (nameIsValid && streetIsValid && postalIsValid && cityIsValid) {
+    formIsValid = true;
+  }
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onConfirm({
+      name: nameValue,
+      city: cityValue,
+      street: streetValue,
+      postal: postalValue,
+    });
+
+    resetName();
+    resetCity();
+    resetPostal();
+    resetStreet();
+  };
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={nameValue}
+        />
+        {nameHasError && <p className="error-text">Please enter a name.</p>}
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input
+          type="text"
+          id="street"
+          onChange={streetChangeHandler}
+          onBlur={streetBlurHandler}
+          value={streetValue}
+        />
+        {streetHasError && <p>Please enter a valid street!</p>}
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input
+          type="text"
+          id="postal"
+          onChange={postalChangeHandler}
+          onBlur={postalBlurHandler}
+          value={postalValue}
+        />
+        {postalHasError && (
+          <p>Please enter a valid postal code (5 characters long)!</p>
+        )}
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          id="city"
+          onChange={cityChangeHandler}
+          onBlur={cityBlurHandler}
+          value={cityValue}
+        />
+        {cityHasError && <p>Please enter a valid city!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
