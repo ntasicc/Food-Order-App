1dcbb226682417df9ec656d6756909473a17085f
ntasicc <ntasicc@gmail.com>
2022-03-27 16:43:56 +0200

    Context Provider removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
-import CartProvider from "./store/CartProvider";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -16,13 +15,13 @@ function App() {
   };
 
   return (
-    <CartProvider>
+    <>
       {showCart && <Cart onClose={closeCartHandler} />}
       <Header onShowCartClick={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </CartProvider>
+    </>
   );
 }
 
