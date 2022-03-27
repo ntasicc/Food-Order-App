be62214161abd3bd4cd466c393369f4cba58752f
ntasicc <ntasicc@gmail.com>
2022-03-27 18:12:58 +0200

    Fetching (Adding) data from (in) session storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCartData } from "./store/cart-actions";
+
+let initialRender = true;
 
 function App() {
+  const dispatch = useDispatch();
   const [showCart, setShowCart] = useState(false);
-
+  const cartData = useSelector((state) => state.cart);
   const showCartHandler = () => {
     setShowCart(true);
   };
@@ -13,6 +18,18 @@ function App() {
   const closeCartHandler = () => {
     setShowCart(false);
   };
+
+  useEffect(() => {
+    if (initialRender) {
+      initialRender = false;
+      return;
+    }
+    sessionStorage.setItem("cartData", JSON.stringify(cartData));
+  }, [cartData.items]);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   return (
     <>
