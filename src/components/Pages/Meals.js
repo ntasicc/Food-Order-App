cd6b903109a58b22ef76685109feb9a13479b00c
ntasicc <ntasicc@gmail.com>
2022-04-12 16:42:50 +0200

    Moved to new folder

---
+++ src/components/Pages/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from "../Meals/AvailableMeals";
+import MealsSummary from "../Meals/MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
