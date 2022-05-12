abb3ceea103769e37fc05aa175a69d6df444c8a8
ntasicc <ntasicc@gmail.com>
2022-05-12 17:52:46 +0200

    Auth reducer added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./cart-slice";
+import authSlice from "./auth/auth-slice";
+import cartSlice from "./cart/cart-slice";
 
 const store = configureStore({
-  reducer: { cart: cartSlice.reducer },
+  reducer: { cart: cartSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
