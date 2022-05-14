e9233543239a96d148153755e6edfbc28a3dd63f
ntasicc <ntasicc@gmail.com>
2022-05-14 17:04:03 +0200

    Sign in and sign out functionality added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,8 +2,24 @@ import classes from "./Header.module.css";
 import mealsImg from "../../assets/m5.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { authActions } from "../../store/auth/auth-slice";
 
 const Header = (props) => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const signInHandler = () => {
+    history.push("/auth");
+  };
+
+  const signOutHandler = () => {
+    dispatch(authActions.logout());
+    history.replace("/");
+  };
+
   return (
     <>
       <header className={classes.header}>
@@ -20,14 +36,17 @@ const Header = (props) => {
                 About
               </NavLink>
             </li>
-            <li>
-              <NavLink to="/auth" activeClassName={classes.active}>
-                Test Auth
-              </NavLink>
-            </li>
           </ul>
         </nav>
-        <HeaderCartButton onClick={props.onShowCartClick} />
+        <div className={classes.buttons}>
+          <HeaderCartButton onClick={props.onShowCartClick} />
+          <button
+            className={classes.logButton}
+            onClick={!isLoggedIn ? signInHandler : signOutHandler}
+          >
+            {!isLoggedIn ? "Sign in" : "Sign out"}
+          </button>
+        </div>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of food!" />
