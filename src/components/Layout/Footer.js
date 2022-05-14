8230e5d6f3fe85153c7a2790e654d03944c4038e
ntasicc <ntasicc@gmail.com>
2022-05-14 17:02:41 +0200

    Footer component

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      Copyright © 2022 Nikola Tasic. All rights reserved. Version: 1.0.0.0
+    </footer>
+  );
+};
+
+export default Footer;
